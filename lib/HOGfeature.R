967ed44e23e79803b94b709e0c0b9484f4368419
yujiehu <31634021+yujiehu@users.noreply.github.com>
2018-03-01 10:29:28 -0500

    3/1/2018 10:28am
    HOG feature added

--- lib/HOGfeature.R
+++ lib/HOGfeature.R
@@ -6,20 +6,7 @@
 ### Project 2
 ### ADS Spring 2018
 
-
-#However, the usual strategy to deal with images of different sizes is to resize them
-#to have the same size. This will allow us to deal with them in a uniform manner, and
-#potentially reduce the amount of required computation.
-
-img_resized = pblapply(imgs, function(img) { resize(img, 128, 128) })
-dim(img_resized[[1]])
-
-#Resizing images is often an appropriate way to deal with images of various sizes.
-#However, we should be careful with the aspect ratios. Indeed, if we distort it
-#too much the image may become hard to recognise. A common strategy is to first
-#crop the image to a square image, then resize the image.
-
-feature <- function(img_dir, set_name, data_name="data", export=T){
+feature <- function(img_dir, export=T){
   
   ### Construct process features for training/testing images
   ### Sample simple feature: Extract row average raw pixel values as features
@@ -28,52 +15,41 @@ feature <- function(img_dir, set_name, data_name="data", export=T){
   ### Output: an .RData file contains processed features for the images
   
   ### load libraries
+  library(reticulate)
+  # If you are using anaconda, point reticulate to the correct conda environment
+  #use_condaenv('/anaconda3/bin/python')
+  use_python('/anaconda3/bin/python')
+  # for some reason I need to import cv2 and tensorflow before EBImage
+  # or everything breaks.
+  cv2 <- reticulate::import('cv2')  
   library("EBImage")
   
   n_files <- length(list.files(img_dir))
   
-  ### determine img dimensions
-  img0 <-  readImage(paste0(img_dir, "img", "_", data_name, "_", set_name, "_", 1, ".jpg"))
-  mat1 <- as.matrix(img0)
-  n_r  <- nrow(img0)
+  winSize <- tuple(64L,64L)
+  blockSize <- tuple(16L,16L)
+  blockStride <- tuple(8L,8L)
+  cellSize <- tuple(8L,8L)
+  nbins = 9L
+  hog = cv2$HOGDescriptor(winSize,blockSize,blockStride,cellSize,nbins)
   
-  ### store vectorized pixel values of images
-  dat <- matrix(NA, n_files, n_r) 
+  imgs <- vector("list", n_files)
+  img_resized<- vector("list", n_files)
+  hog_values <- matrix(NA, 1764,n_files ) 
+  
   for(i in 1:n_files){
-    img     <- readImage(paste0(img_dir,  "img", "_", data_name, "_", set_name, "_", i, ".jpg"))
-    dat[i,] <- rowMeans(img)
+    img <- readImage(paste0(img_dir,  "pet", i, ".jpg"))
+    imgs[[i]] <- img
+    img_resized[[i]] <- cv2$resize(imgs[[i]], dsize=tuple(64L, 64L))
+    ### store vectorized pixel values of images
+    hog_values[,i] <- as.vector(hog$compute(np_array(img_resized[[i]] * 255, dtype='uint8')))
   }
   
   ### output constructed features
   if(export){
-    save(dat, file = paste0("../output/feature_", data_name, "_", set_name, ".RData"))
+    hog_value<-t(hog_values)
+    save(hog_value, file = paste0("../output/feature_",  "HOG", ".RData"))
   }
   return(dat)
 }
 
-
-
-## HOG (Histogram of oriented gradients)
-
-# We create a HOG object
-# The only parameter of real importance here is winSize,
-# but we are required to pass in at least this many parameters
-# so that OpenCV can figure out which function we want to call.
-
-winSize <- tuple(64L,64L)
-blockSize <- tuple(16L,16L)
-blockStride <- tuple(8L,8L)
-cellSize <- tuple(8L,8L)
-nbins = 9L
-
-hog = cv2$HOGDescriptor(winSize,blockSize,blockStride,cellSize,nbins)
-
-#Most algorithms only work over images of fixed sizes. Let us resize the image
-#and compute the hog descriptor on the image. We note that the HOG descriptor
-#only accepts inputs in the 0-255 uint8 format, and so we need to specify
-#this as our input type.
-img_resized <- cv2$resize(img, dsize=tuple(64L, 64L))
-hog_values <- hog$compute(np_array(img_resized * 255, dtype='uint8'))
-
-#We thus compute a feature vector of length 1764 (in this case) which corresponds
-#to the HOG features of our image.
